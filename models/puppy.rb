900e3d929650c0751ed1b4c0f0f713f128ba8110
Michael Nnaji <mnnaji3@uwo.ca>
2021-01-22 03:28:54 +0000

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,18 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+
+class Puppy 
+
+attr_accessor :name, :age, :breed
+
+def initialize args
+    args.each do |k,v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
+  end
+
+
+
+
+
+
+end  
