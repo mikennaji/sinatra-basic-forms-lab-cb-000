94c1aba6d967743ab1139c331f63f681e9ce4ff5
Michael Nnaji <mnnaji3@uwo.ca>
2021-01-22 03:24:29 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -4,12 +4,11 @@ class Puppy
 
 attr_accessor :name, :age, :breed
 
-def initialize(name, breed, age)
-  @name = name 
-  @breed = breed 
-  @age = age 
-end 
-
+def initialize args
+    args.each do |k,v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
+  end
 
 
 
